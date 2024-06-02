ca7209d042093eac49517d68441c06cf26f188d4
SuXYIO <SuXYMC@163.com>
2024-06-02 17:45:25 +0800

    Improve day-night colorscheme cycle.

--- lua/init.lua
+++ lua/init.lua
@@ -5,10 +5,34 @@ local time = tonumber(os.date("%H", os.time()))
 if (time > 5 and time <= 17)
 then
 	-- day
+	vim.cmd('colorscheme gruvbox')
 	vim.o.background = 'light'
 end
 if (time > 17 or time <= 5)
 then
 	-- night
+	vim.cmd('colorscheme ayu')
+	vim.o.background = 'dark'
+end
+
+if (time > 4 and time <= 10)
+then
+	-- morning
+	vim.cmd('colorscheme gruvbox')
+	vim.o.background = 'light'
+elseif (time > 10 and time <= 16)
+then
+	-- noon
+	vim.cmd('colorscheme ayu')
+	vim.o.background = 'light'
+elseif (time > 16 and time <= 22)
+then
+	-- afternoon
+	vim.cmd('colorscheme ayu')
+	vim.o.background = 'dark'
+elseif (time > 22 or time <= 4)
+then
+	-- midnight
+	vim.cmd('colorscheme gruvbox')
 	vim.o.background = 'dark'
 end
