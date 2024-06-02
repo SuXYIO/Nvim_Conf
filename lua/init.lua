7282588b75b7530778e070ca1dc185ba008bde87
SuXYIO <SuXYMC@163.com>
2024-06-02 22:33:55 +0800

    Change time ranges for day-night color change.

--- lua/init.lua
+++ lua/init.lua
@@ -2,22 +2,22 @@ require('plugins')
 
 -- set background based on time
 local time = tonumber(os.date("%H", os.time()))
-if (time > 4 and time <= 10)
+if (time > 3 and time <= 9)
 then
 	-- morning
 	vim.cmd('colorscheme gruvbox')
 	vim.o.background = 'light'
-elseif (time > 10 and time <= 16)
+elseif (time > 9 and time <= 15)
 then
 	-- noon
 	vim.cmd('colorscheme ayu')
 	vim.o.background = 'light'
-elseif (time > 16 and time <= 22)
+elseif (time > 15 and time <= 21)
 then
 	-- afternoon
 	vim.cmd('colorscheme ayu')
 	vim.o.background = 'dark'
-elseif (time > 22 or time <= 4)
+elseif (time > 21 or time <= 3)
 then
 	-- midnight
 	vim.cmd('colorscheme gruvbox')
