c68411d012c2145df9a3c5271784df718d208c84
SuXYIO <SuXYMC@163.com>
2024-06-21 21:11:53 +0800

    Update keymappings.

--- lua/init.lua
+++ lua/init.lua
@@ -1,25 +1,3 @@
 require('plugins')
-
--- set background based on time
-local time = tonumber(os.date("%H", os.time()))
-if (time > 3 and time <= 9)
-then
-	-- morning
-	vim.cmd('colorscheme gruvbox')
-	vim.o.background = 'light'
-elseif (time > 9 and time <= 15)
-then
-	-- noon
-	vim.cmd('colorscheme ayu')
-	vim.o.background = 'light'
-elseif (time > 15 and time <= 21)
-then
-	-- afternoon
-	vim.cmd('colorscheme ayu')
-	vim.o.background = 'dark'
-elseif (time > 21 or time <= 3)
-then
-	-- midnight
-	vim.cmd('colorscheme gruvbox')
-	vim.o.background = 'dark'
-end
+require('lspconf').setup()
+require('daynightcycle')
