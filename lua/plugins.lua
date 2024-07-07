0035f03458d8d0f8ef1af7e3cf5a97cb787114c9
SuXYIO <SuXYMC@163.com>
2024-07-07 11:53:59 +0800

    Add cmp_plug.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,22 @@
 -- Lazyvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plug
-	local nvim_treesitter_plug = {"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
+	local nvim_treesitter_plug = {'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
 		config = function ()
-			local configs = require("nvim-treesitter.configs")
+			local configs = require('nvim-treesitter.configs')
 			configs.setup({
 				ensure_installed = { 'c', 'markdown', 'lua', 'vim', 'vimdoc' },
 				sync_install = false,
@@ -28,12 +28,12 @@ vim.opt.rtp:prepend(lazypath)
 	local nvim_tree_plug = {'nvim-tree/nvim-tree.lua', 
 		dependencies = 'nvim-tree/nvim-web-devicons',
 		keys = {
-			{"<C-n>", "<CMD>NvimTreeToggle<CR>"}
+			{'<C-n>', '<CMD>NvimTreeToggle<CR>'}
 		},
 		config = function ()
-			require("nvim-tree").setup({
+			require('nvim-tree').setup({
 				sort = {
-					sorter = "case_sensitive",
+					sorter = 'case_sensitive',
 				},
 				view = {
 					width = 30,
@@ -51,25 +51,25 @@ vim.opt.rtp:prepend(lazypath)
 		lazy = true
 	}
 	local todo_comments_plug = {'folke/todo-comments.nvim', 
-		dependencies = "nvim-lua/plenary.nvim",
+		dependencies = 'nvim-lua/plenary.nvim',
 		config = function()
-			require("todo-comments").setup {
+			require('todo-comments').setup {
 				signs = true, -- show icons in the signs column
 				sign_priority = 8, -- sign priority
 				-- keywords recognized as todo comments
 				keywords = {
 					FIX = {
-						icon = " ", -- icon used for the sign, and in search results
-						color = "error", -- can be a hex color, or a named color (see below)
-						alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
+						icon = ' ', -- icon used for the sign, and in search results
+						color = 'error', -- can be a hex color, or a named color (see below)
+						alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'ERROR' }, -- a set of other keywords that all map to this FIX keywords
 						-- signs = false, -- configure signs for some keywords individually
 					},
-					TODO = { icon = "󰉹 ", color = "info" },
-					HACK = { icon = " ", color = "warning" },
-					WARN = { icon = " ", color = "warning", alt = { "WARNING" } },
-					PERF = { icon = " ", color = "hint", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-					NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-					TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+					TODO = { icon = '󰉹 ', color = 'info' },
+					HACK = { icon = ' ', color = 'warning' },
+					WARN = { icon = ' ', color = 'warning', alt = { 'WARNING' } },
+					PERF = { icon = ' ', color = 'hint', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+					NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+					TEST = { icon = ' ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
 				},
 				 -- Test Style
 				 -- FIX: Test Style
@@ -80,8 +80,8 @@ vim.opt.rtp:prepend(lazypath)
 				 -- NOTE: Test Style
 				 -- TEST: Test Style
 				gui_style = {
-					fg = "NONE", -- The gui style to use for the fg highlight group.
-					bg = "BOLD", -- The gui style to use for the bg highlight group.
+					fg = 'NONE', -- The gui style to use for the fg highlight group.
+					bg = 'BOLD', -- The gui style to use for the bg highlight group.
 				},
 				merge_keywords = true, -- when true, custom keywords will be merged with the defaults
 				-- highlighting of the line containing the todo comment
@@ -90,11 +90,11 @@ vim.opt.rtp:prepend(lazypath)
 				-- * after: highlights after the keyword (todo text)
 				highlight = {
 					multiline = true, -- enable multine todo comments
-					multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+					multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
 					multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-					before = "", -- "fg" or "bg" or empty
-					keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-					after = "fg", -- "fg" or "bg" or empty
+					before = '', -- 'fg' or 'bg' or empty
+					keyword = 'wide', -- 'fg', 'bg', 'wide', 'wide_bg', 'wide_fg' or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+					after = 'fg', -- 'fg' or 'bg' or empty
 					pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
 					comments_only = true, -- uses treesitter to match keywords in comments only
 					max_line_len = 400, -- ignore lines longer than this
@@ -103,21 +103,21 @@ vim.opt.rtp:prepend(lazypath)
 				-- list of named colors where we try to extract the guifg from the
 				-- list of highlight groups or use the hex color if hl not found as a fallback
 				colors = {
-					error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-					warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-					info = { "DiagnosticInfo", "#2563EB" },
-					hint = { "DiagnosticHint", "#10B981" },
-					default = { "Identifier", "#7C3AED" },
-					test = { "Identifier", "#FF00FF" }
+					error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+					warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+					info = { 'DiagnosticInfo', '#2563EB' },
+					hint = { 'DiagnosticHint', '#10B981' },
+					default = { 'Identifier', '#7C3AED' },
+					test = { 'Identifier', '#FF00FF' }
 				},
 				search = {
-					command = "rg",
+					command = 'rg',
 					args = {
-						"--color=never",
-						"--no-heading",
-						"--with-filename",
-						"--line-number",
-						"--column",
+						'--color=never',
+						'--no-heading',
+						'--with-filename',
+						'--line-number',
+						'--column',
 					},
 					-- regex that will be used to match keywords.
 					-- don't replace the (KEYWORDS) placeholder
@@ -133,7 +133,7 @@ vim.opt.rtp:prepend(lazypath)
 	local telescope_plug = {'nvim-telescope/telescope.nvim',
 		dependencies = 'nvim-lua/plenary.nvim',
 		keys = {
-			{"ff", "<CMD>Telescope find_files<CR>"}
+			{'ff', '<CMD>Telescope find_files<CR>'}
 		}
 	}
 	local ale_plug = {'dense-analysis/ale',
@@ -185,7 +185,7 @@ vim.opt.rtp:prepend(lazypath)
 					lualine_z = {}
 				},
 				tabline = {
-					lualine_a = {"os.date('%Y-%m-%d | %H:%M:%S | %a',os.time())", 'data', "require'lsp-status'.status()"},
+					lualine_a = {'os.date("%Y-%m-%d | %H:%M:%S | %a",os.time())', 'data', 'require\'lsp-status\'.status()'},
 					lualine_b = {},
 					lualine_c = {},
 					lualine_x = {},
@@ -213,12 +213,12 @@ vim.opt.rtp:prepend(lazypath)
 				theme = 'hyper',
 				config = {
 					header = {
-						' _______             ____   ____.__         ',
-						' \\      \\   ____  ___\\   \\ /   /|__| _____  ',
-						' /   |   \\_/ __ \\/  _ \\   Y   / |  |/     \\ ',
-						'/    |    \\  ___(  <_> )     /  |  |  Y Y  \\',
-						'\\____|__  /\\___  >____/ \\___/   |__|__|_|  /',
-						'        \\/     \\/                        \\/ '
+						' _______			       ____   ____.__         ',
+						' \\			\\   ____  ___\\   \\ /   /|__| _____  ',
+						' /	 |	 \\_/ __ \\/	_ \\   Y   / |  |/     \\ ',
+						'/		|	  \\  ___(  <_> )     /  |  |  Y Y  \\',
+						'\\____|__	/\\___	>____/ \\___/	 |__|__|_|  /',
+						'			  \\/     \\/                        \\/ '
 					},
 					week_header = {enable = false},
 					shortcut = {
@@ -234,21 +234,49 @@ vim.opt.rtp:prepend(lazypath)
 	}
 	local lazygit_plug = {'kdheepak/lazygit.nvim',
 		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
+			'LazyGit',
+			'LazyGitConfig',
+			'LazyGitCurrentFile',
+			'LazyGitFilter',
+			'LazyGitFilterCurrentFile',
 		},
 		-- optional for floating window border decoration
 		dependencies = {
-			"nvim-lua/plenary.nvim",
+			'nvim-lua/plenary.nvim',
 		},
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{"<C-g>", "<cmd>LazyGit<cr>", desc = "LazyGit"}
+			{'<C-g>', '<cmd>LazyGit<cr>', desc = 'LazyGit'}
 		}
+	}
+	local cmp_plug = {'hrsh7th/nvim-cmp',
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+		},
+		config = function()
+			cmp = require('cmp')
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<CR>'] = cmp.mapping.confirm({select = true}),
+				}),
+				sources = cmp.config.sources({
+						{name = 'nvim_lsp'},
+					},
+					{
+						{name = 'buffer'},
+				})
+			})
+		end,
 	}
 -- Colorscheme
 	-- main colorscheme
@@ -258,32 +286,30 @@ vim.opt.rtp:prepend(lazypath)
 		config = function()
 			vim.cmd([[colorscheme ayu]])
 			-- set ayucolor based on time
-			local time = tonumber(os.date("%H", os.time()))
+			local time = tonumber(os.date('%H', os.time()))
 			if (time > 2 and time <= 8)
 			then
 				-- morning
-				vim.cmd('let ayucolor="mirage"')
+				vim.cmd('let ayucolor=\'mirage\'')
 			elseif (time > 8 and time <= 14)
 			then
 				-- noon
-				vim.cmd('let ayucolor="light"')
+				vim.cmd('let ayucolor=\'light\'')
 			elseif (time > 14 and time <= 20)
 			then
 				-- afternoon
-				vim.cmd('let ayucolor="mirage"')
+				vim.cmd('let ayucolor=\'mirage\'')
 			elseif (time > 20 or time <= 2)
 			then
 				-- midnight
-				vim.cmd('let ayucolor="dark"')
+				vim.cmd('let ayucolor=\'dark\'')
 			end
 		end
 	}
 	-- other colorschemes
 	local gruvbox_colorscheme = {'morhetz/gruvbox', lazy = false}
-	-- lazy colorschemes
-	local papercolor_colorscheme = {'NLKNguyen/papercolor-theme', lazy = true}
 
-require("lazy").setup({
+require('lazy').setup({
 	-- plug
 	nvim_treesitter_plug,
 	nvim_tree_plug,
@@ -295,6 +321,7 @@ require("lazy").setup({
 	lualine_plug,
 	dashboard_plug,
 	lazygit_plug,
+	cmp_plug,
 	-- colorscheme
 	ayu_colorscheme,
 	gruvbox_colorscheme,
