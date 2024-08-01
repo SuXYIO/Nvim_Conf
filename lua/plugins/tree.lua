026ae988d9c1310a2e7111e7406e570002195ffc
SuXY <SuXYMC@163.com>
2024-08-01 15:37:04 +0800

    Add autochdir support for base and tree, add cwd lualine.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,10 +1,12 @@
 Tree = {'nvim-tree/nvim-tree.lua',
 	dependencies = 'nvim-tree/nvim-web-devicons',
+	cmd = {'NvimTreeToggle'},
 	keys = {
 		{'<Leader>f', '<CMD>NvimTreeToggle<CR>'}
 	},
 	config = function()
 		require('nvim-tree').setup({
+			respect_buf_cwd = true,
 			git = {
 				enable = true,
 			},
@@ -22,5 +24,6 @@ Tree = {'nvim-tree/nvim-tree.lua',
 				enable = true,
 			}
 		})
+		vim.keymap.set('n', '<Leader>f', '<CMD>NvimTreeToggle<CR>')
 	end
 }
