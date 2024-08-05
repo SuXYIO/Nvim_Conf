67b267a0e752603b3a1b996c94192d0b6a53c3f5
SuXY <SuXYMC@163.com>
2024-08-05 10:29:55 +0800

    Add todo jump keymap.

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -17,6 +17,7 @@ Lspsaga = {'nvimdev/lspsaga.nvim',
 		vim.keymap.set('n', '[E', '<CMD>Lspsaga diagnostic_jump_prev<CR>')
 		vim.keymap.set('n', '<Leader>s', '<CMD>Lspsaga outline<CR>')
 		vim.keymap.set('n', '<Leader>a', '<CMD>Lspsaga code_action<CR>')
+		vim.keymap.set('n', '<Leader>v', '<CMD>Lspsaga peek_definition<CR>')
 	end,
 	dependencies = {
 		'nvim-tree/nvim-web-devicons',
