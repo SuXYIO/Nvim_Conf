7ba5eebd31f5ec4acfc0cc94345f752c9f88ed46
SuXYIO <SuXYMC@163.com>
2024-10-24 20:49:04 +0800

    Update bufferline & lualine config, add new linesep.

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,8 @@ return {'rcarriga/nvim-notify',
 	event = 'VeryLazy',
 	config = function()
 		require("notify").setup({
-			stages = 'fade_in_slide_out'
+			stages = 'fade_in_slide_out',
+			background_color = '#000000'
 		})
 		vim.notify = require("notify")
 	end,
