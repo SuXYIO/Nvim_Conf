558f942c3e487a4b2cb6d488b0365efe1605b9bc
SueXY <SuXYMC@163.com>
2025-02-04 10:08:34 +0800

    Remove redundant files

--- lua/everforest.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sainnhe/everforest",
-	lazy = true,
-	config = function()
-		vim.g.everforest_enable_italic = true
-		vim.g.everforest_background = "medium"
-		vim.cmd("colorscheme everforest")
-	end,
-}
--- lua/material.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"marko-cerovac/material.nvim",
-	lazy = true,
-	config = function()
-		vim.cmd("colorscheme material")
-	end,
-}
