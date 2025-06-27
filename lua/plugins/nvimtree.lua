a801a4f7192cf033a4b0c01efe2e7862df34ee32
SueXY <SuXYMC@163.com>
2025-06-27 11:06:37 +0800

    Modify nvimtree config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -5,31 +5,20 @@ return {
 	keys = {
 		{ "<Leader>f", "<CMD>NvimTreeToggle<CR>", mode = "n", noremap = true, desc = "toggle nvim tree" },
 	},
-	config = function()
-		require("nvim-tree").setup({
-			respect_buf_cwd = true,
-			git = {
+	opts = {
+		hijack_cursor = true,
+		respect_buf_cwd = true,
+		diagnostics = {
+			enable = true,
+		},
+		renderer = {
+			indent_markers = {
 				enable = true,
 			},
-			view = {
-				side = "left",
-				number = false,
-				relativenumber = false,
-				signcolumn = "yes",
-				width = 30,
-			},
-			renderer = {
-				group_empty = true,
-			},
-			diagnostics = {
-				enable = true,
-			},
-			renderer = {
-				indent_markers = {
-					enable = true,
-				},
-			},
-		})
-		vim.keymap.set("n", "<Leader>f", "<CMD>NvimTreeToggle<CR>", { noremap = true, desc = "nvimtree toggle" })
-	end,
+		},
+		filters = {
+			git_ignored = false,
+			custom = { ".git", ".DS_Store" },
+		},
+	},
 }
