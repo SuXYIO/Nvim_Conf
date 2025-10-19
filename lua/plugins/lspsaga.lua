aeb4593ae1d5d7c6f2a5dc3114b440b86150fe93
SueXY <SuXYMC@163.com>
2025-10-19 22:39:08 +0800

    Add ufo

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -9,8 +9,7 @@ return {
 		{ "]e", "<CMD>Lspsaga diagnostic_jump_next<CR>", mode = "n", noremap = true, desc = "next diagnostic" },
 		{ "[e", "<CMD>Lspsaga diagnostic_jump_prev<CR>", mode = "n", noremap = true, desc = "previous diagnostic" },
 		{ "<Leader>s", "<CMD>Lspsaga outline<CR>", mode = "n", noremap = true, desc = "lsp outline" },
-		{ "<Leader>a", "<CMD>Lspsaga code_action<CR>", mode = "n", noremap = true, desc = "lsp code action" },
-		{ "<Leader>v", "<CMD>Lspsaga peek_definition<CR>", mode = "n", noremap = true, desc = "lsp peek definition" },
+		{ "<Leader>a", "<CMD>Lspsaga peek_definition<CR>", mode = "n", noremap = true, desc = "lsp peek definition" },
 	},
 	opts = {
 		ui = {
