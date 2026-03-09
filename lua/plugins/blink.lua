4066eb963c5ed391084750d3ac994a8977a62420
SueXY <SuXYMC@163.com>
2026-03-10 00:13:29 +0800

    Add wakatime, add blink show menu bind

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,12 +4,13 @@ return {
 	opts = {
 		keymap = {
 			preset = "enter",
-			["<Tab>"] = { "select_next", "fallback" },
+			["<Tab>"] = { "show", "select_next", "fallback" },
 			["<S-Tab>"] = { "select_prev", "fallback" },
 		},
 		fuzzy = { implementation = "lua" },
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			menu = { auto_show = true },
 		},
 	},
 }
